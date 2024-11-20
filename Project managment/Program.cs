c8fb91cbd68d6f03039bb34a5a831867eeff4582
Ivanko1257 <ivan@vukusic.net>
2024-11-20 21:37:11 +0100

    Definition of Project and Task

--- Project managment/Program.cs
+++ Project managment/Program.cs
@@ -6,6 +6,6 @@ public static class Program
     public static Dictionary<Project, List<Task>> projects = new Dictionary<Project, List<Task>>();
     public static void Main(string[] args)
     {
-
+        Console.WriteLine("Upišite broj operacije koje želite obaviti:\n1 - Ispis projekta\n2 - Dodavanje projekta\n3 - Brisanje projekta\n4 - Prikaz svih zadataka s rokom u sljedećih 7 dana\n5 - Prikaz projekta po statusu\n6 - Upravljanje projektima\n7 - Upravljanje zadatcima");
     }
 }
