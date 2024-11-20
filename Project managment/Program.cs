edb1ca9c95ce55837893f6f230f94cd8a2912639
Ivanko1257 <ivan@vukusic.net>
2024-11-20 21:30:29 +0100

    Add project files.

---
+++ Project managment/Program.cs
@@ -0,0 +1,11 @@
+﻿using Project_managment;
+using Task = Project_managment.Task;
+
+public static class Program
+{
+    public static Dictionary<Project, List<Task>> projects = new Dictionary<Project, List<Task>>();
+    public static void Main(string[] args)
+    {
+
+    }
+}
---
+++ Project managment/classDeffinition.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_managment
+{
+    public enum ProjectStatus
+    {
+        Active,
+        Standby,
+        Finished
+    }
+    public enum TaskStatus
+    {
+        Active,
+        Finished,
+        Delayed
+    }
+    public class Project
+    {
+        public string projectName;
+        public string projectDescription;
+        public DateTime projectStartDate;
+        public DateTime projectEndDate;
+        public ProjectStatus projectStatus;
+        public Project(string name, string decription, DateTime startDate, DateTime endDate)
+        {
+            projectName = name;
+            projectDescription = decription;
+            projectStartDate = startDate.Date;
+            projectEndDate = endDate.Date;
+            projectStatus = ProjectStatus.Active;
+        }
+    }
+    public class Task
+    {
+        public string taskName;
+        public string taskDescription;
+        public DateTime taskDeadlineDate;
+        public TaskStatus taskStatus;
+        public Project projectParent;
+        public Task(string name, string description, DateTime deadline, Project project)
+        {
+            taskName = name;
+            taskDescription = description;
+            taskDeadlineDate = deadline;
+            taskStatus = TaskStatus.Active;
+        }
+    }
+}
